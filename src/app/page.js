0339287d6c4f3680181c5a5a9cbcb7e610343a2c
Nobuyuki Takahiro <5891401+StudioQuad9@users.noreply.github.com>
2026-07-18 07:56:45 +0900

    rest now

--- src/app/page.js
+++ src/app/page.js
@@ -10,41 +10,44 @@ export default function Home() {
   ];
 
   return (
-    <div className={styles.topPage}>
-      {topBGImg.map((path, idx) => (
-        <div
-          key={path}
-          className={`
-            ${styles.topBackGroundImage} ${idx === 1 ? styles.secondOne : ""}
-          `}
-          style={{ backgroundImage: `url(${path})` }}
-        ></div>
-      ))}
-      <div className={styles.topContent}>
-        <h1 className={styles.title}>Awai</h1>
-        <div className={styles.wrapper}>
-          <p className={styles.catchCopy}>Where something begins between</p>
-          <p className={styles.copyEn}>
-            Quiet cultural experiences&nbsp;
+    <>
+      now printing...
+      {/* <div className={styles.topPage}>
+        {topBGImg.map((path, idx) => (
+          <div
+            key={path}
+            className={`
+              ${styles.topBackGroundImage} ${idx === 1 ? styles.secondOne : ""}
+            `}
+            style={{ backgroundImage: `url(${path})` }}
+          ></div>
+        ))}
+        <div className={styles.topContent}>
+          <h1 className={styles.title}>Awai</h1>
+          <div className={styles.wrapper}>
+            <p className={styles.catchCopy}>Where something begins between</p>
+            <p className={styles.copyEn}>
+              Quiet cultural experiences&nbsp;
+              <br className="br-for-smartphone" />
+              in Kyoto,&nbsp;
+              <br className="br-for-smartphone" />
+              through tea, silence, and reflection
+            </p>
+          </div>
+          <p className={styles.copyJp}>
+            あわい &nbsp;
+            <span className="horizontal-wide">–</span>
+            &nbsp; 静けさのなかで<span className="tume-minus-1">、</span>
             <br className="br-for-smartphone" />
-            in Kyoto,&nbsp;
-            <br className="br-for-smartphone" />
-            through tea, silence, and reflection
+            ひらく体験へ
           </p>
+          <div className={`cta ${styles.topCta}`}>
+            <Link className="btn btn--regular" href="/en/experiences">
+              View Experiences
+            </Link>
+          </div>
         </div>
-        <p className={styles.copyJp}>
-          あわい &nbsp;
-          <span className="horizontal-wide">–</span>
-          &nbsp; 静けさのなかで<span className="tume-minus-1">、</span>
-          <br className="br-for-smartphone" />
-          ひらく体験へ
-        </p>
-        <div className={`cta ${styles.topCta}`}>
-          <Link className="btn btn--regular" href="/en/experiences">
-            View Experiences
-          </Link>
-        </div>
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 }
